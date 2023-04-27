8d11303a38229f96e66d07ce0935da3e6ffdaa14
Sahenulislam <71545997+Sahenulislam@users.noreply.github.com>
2023-04-27 13:20:26 +0600

    Update 1011 - Marriage Ceremonies.cpp

--- Dynamic Programming/1011 - Marriage Ceremonies.cpp
+++ Dynamic Programming/1011 - Marriage Ceremonies.cpp
@@ -1,30 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define printv(x) for(ll i=0;i<x.size();i++){if(i+1==x.size())cout<<x[i]<<endl;else cout<<x[i]<<" ";}
-#define printa(x,n) for(ll i=0;i<n;i++){if(i+1==n)cout<<x[i]<<endl;else cout<<x[i]<<" ";}
 #define fast                          \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
-#define INF        9223372036854775806
-#define MINF       -9223372036854775806
-#define eps          1e-8
-#define gcd __gcd
-#define yes "YES"
-#define no "NO"
-#define pb push_back
-#define si size()
-#define in " "
-#define mone "-1"
-#define zero "0"
-#define one "1"
-#define PI acos(-1)
-const ll mod=1e9+7;
-const double pi=2*acos(0.0);
-ll lcm(ll a,ll b)
-{
-    return (a/(__gcd(a, b)))*b;
-}
 
 const ll mxn=16;
 ll a[mxn+10][mxn+10];
